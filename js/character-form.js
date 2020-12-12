8f7b41dd4f933283aa96396f991cecc8d12b8e70
RekaKR <RekaKR@users.noreply.github.com>
2020-12-12 20:08:40 +0100

    Edit notes and variables.

--- js/character-form.js
+++ js/character-form.js
@@ -1,23 +1,45 @@
 function pageLoad () {
 
+  //character name
   let nameSelect = document.getElementById("name-select");
-  let race = document.getElementById("race");
+  
+  //race
+  let raceSelect = document.getElementById("race-select");
+  let raceSelectValue = raceSelect.options[recaSelect.selectedIndex].value;
+  let raceSelectText = raceSelect.options[raceSelect.selectedIndex].text;
+  
+  /*
   let raceChoose = document.getElementById("race-choose");
   let pink = document.getElementById("pink");
   let red = document.getElementById("red");
   let orange = document.getElementById("orange");
   let yellow = document.getElementById("yellow");
   let white = document.getElementById("white");
+  */
+
+  //gender
   let genderSelect = document.getElementById("gender-select");
+  let genderSelectValue = raceSelect.options[genderSelect.selectedIndex].value;
+  let genderSelectText = raceSelect.options[genderSelect.selectedIndex].text;
+  
+  /*
   let genderChoose = document.getElementById("gender-choose");
   let female = document.getElementById("female");
   let male = document.getElementById("male");
+  */
+
+  //class
   let classSelect = document.getElementById("class-select");
+  let classSelectValue = raceSelect.options[classSelect.selectedIndex].value;
+  let classSelectText = raceSelect.options[classSelect.selectedIndex].text;
+  
+  /*
   let classChoose = document.getElementById("class-choose");
   let fighter = document.getElementById("fighter");
   let noob = document.getElementById("noob");
   let innocent = document.getElementById("innocent");
-
+  */
+  
 
   /*
   document.getElementById("#id").addEventListener('esemény', function());
