8e9ac7c04b1c2e0b3bb029ff1ebb8cfbeca60638
RekaKR <RekaKR@users.noreply.github.com>
2020-12-12 19:36:30 +0100

    Create variables.

--- js/character-form.js
+++ js/character-form.js
@@ -1,7 +1,23 @@
 function pageLoad () {
 
+  let nameSelect = document.getElementById("name-select");
+  let race = document.getElementById("race");
+  let raceChoose = document.getElementById("race-choose");
+  let pink = document.getElementById("pink");
+  let red = document.getElementById("red");
+  let orange = document.getElementById("orange");
+  let yellow = document.getElementById("yellow");
+  let white = document.getElementById("white");
+  let genderSelect = document.getElementById("gender-select");
+  let genderChoose = document.getElementById("gender-choose");
+  let female = document.getElementById("female");
+  let male = document.getElementById("male");
+  let classSelect = document.getElementById("class-select");
+  let classChoose = document.getElementById("class-choose");
+  let fighter = document.getElementById("fighter");
+  let noob = document.getElementById("noob");
+  let innocent = document.getElementById("innocent");
 
-  
 
   /*
   document.getElementById("#id").addEventListener('esemény', function());
