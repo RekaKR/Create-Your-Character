73989640b75c2fb36acc0f2274a3604e8ca903a6
RekaKR <RekaKR@users.noreply.github.com>
2020-12-13 10:53:11 +0100

    Write the structure of the image change if else.

--- js/character-form.js
+++ js/character-form.js
@@ -6,6 +6,7 @@ function pageLoad() {
   `);
 
 
+
   //ch text bar
   let text = document.getElementById("text");
 
@@ -16,10 +17,7 @@ function pageLoad() {
   let divs = text.querySelectorAll("div");
 
 
-  //ch img
-  text.insertAdjacentHTML('beforebegin', `<img src="../img/red-among-us-noob.png" alt="Red character noob">`);
 
-  
   //ch name
   let chName = document.getElementById("ch-name");
 
@@ -28,6 +26,7 @@ function pageLoad() {
   }
 
   chName.addEventListener('input', showChName);
+
 
 
   //race
@@ -40,6 +39,7 @@ function pageLoad() {
   raceSelect.addEventListener('change', racePrint);
 
 
+
   //gender
   let genderSelect = document.getElementById("gender-select");
 
@@ -48,6 +48,7 @@ function pageLoad() {
   }
 
   genderSelect.addEventListener('change', genderPrint);
+
 
 
   //class
@@ -59,6 +60,51 @@ function pageLoad() {
 
   classSelect.addEventListener('change', classPrint);
 
+
+
+  //NEM MŰKÖDIK MÉG
+  //ch img
+  if (raceSelect.value === "") {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/red-among-us-noob.png" alt="Red character noob">`);
+  }
+
+/*
+  if (race === pink && class. === fighter) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/pink-among-us-police.png" alt="Pink character fighter">`);
+  } else if (race === pink && class. === innocent) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/pink-among-us-innocent.png" alt="Pink character innocent">`);
+  } else if (race === pink && class. === "" || race === pink && class. === noob) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/pink-among-us-noob.png" alt="Pink character noob">`);
+  
+  } else if (race === red && class. === fighter) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/red-among-us-police.png" alt="Red character fighter">`);
+  } else if (race === red && class. === innocent) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/red-among-us-innocent.png" alt="Red character innocent">`);
+
+  } else if (race === orange && class. === fighter) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/orange-among-us-police.png" alt="Orange character fighter">`);
+  } else if (race === orange && class. === innocent) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/orange-among-us-innocent.png" alt="Orange character innocent">`);
+  } else if (race === orange && class. === "" || race === orange && class. === noob) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/orange-among-us-noob.png" alt="Orange character noob">`);
+  
+  } else if (race === yellow && class. === fighter) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/yellow-among-us-police.png" alt="Yellow character fighter">`);
+  } else if (race === yellow && class. === innocent) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/yellow-among-us-innocent.png" alt="Yellow character innocent">`);
+  } else if (race === yellow && class. === "" || race === yellow && class. === noob) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/yellow-among-us-noob.png" alt="Yellow character noob">`);
+
+  } else if (race === white && class. === fighter) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/white-among-us-police.png" alt="White character fighter">`);
+  } else if (race === white && class. === innocent) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/white-among-us-innocent.png" alt="White character innocent">`);
+  } else if (race === white && class. === "" || race === white && class. === noob) {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/white-among-us-noob.png" alt="White character noob">`);
+  } else {
+    text.insertAdjacentHTML('beforebegin', `<img src="../img/red-among-us-noob.png" alt="Red character noob">`);
+  }
+*/
 
   //submit variable
   //let submit = document.getElementById('submit');
