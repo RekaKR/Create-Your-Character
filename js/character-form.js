6742081eb7f31d4073d29c02aa3de9595722eea7
RekaKR <RekaKR@users.noreply.github.com>
2020-12-13 09:33:33 +0100

    Edit js structure.

--- js/character-form.js
+++ js/character-form.js
@@ -1,45 +1,51 @@
-function pageLoad () {
+function pageLoad() {
   
-  //content variable
+  //top pic
+  document.querySelector("body").insertAdjacentHTML('afterbegin', `
+  <img class="main-sign" src="../img/among-us-sign.png" alt="">
+  `);
+
+
+  //text bar
   let text = document.getElementById("text");
+
+  for (let i = 0; i < 4; i++) {
+    text.insertAdjacentHTML('beforeend', `<div></div>`);
+  }
+
   let divs = text.querySelectorAll("div");
 
-  //ch name variable
-  let chName = document.getElementById("ch-name");  
 
-  //race variable
+  //ch name
+  let chName = document.getElementById("ch-name");
+
+  function showChName() {
+    divs[0].textContent = chName.value;
+  }
+
+  chName.addEventListener('input', showChName);
+
+
+  //race
   let raceSelect = document.getElementById("race-select");
   let raceSelectValue = raceSelect.options[raceSelect.selectedIndex].value;
   let raceSelectText = raceSelect.options[raceSelect.selectedIndex].text;
 
-  //gender variable
+  function racePrint() {
+    divs[1].insertAdjacentHTML('beforeend', `
+        <p>${raceSelectText}</p>
+        <p>${raceSelectValue}</p>
+      `);
+  }
+
+  raceSelect.addEventListener('change', racePrint);
+
+
+  //gender
   let genderSelect = document.getElementById("gender-select");
   let genderSelectValue = genderSelect.options[genderSelect.selectedIndex].value;
   let genderSelectText = genderSelect.options[genderSelect.selectedIndex].text;
 
-  //class variable
-  let classSelect = document.getElementById("class-select");
-  let classSelectValue = classSelect.options[classSelect.selectedIndex].value;
-  let classSelectText = classSelect.options[classSelect.selectedIndex].text;
-  
-  //submit variable
-  //let submit = document.getElementById('submit');
-
-
-  //ch name func.
-  function showChName() {
-    divs[0].textContent = chName.value;
-  }
-  
-  //race func.
-  function racePrint() {
-    divs[1].insertAdjacentHTML('beforeend', `
-      <p>${raceSelectText}</p>
-      <p>${raceSelectValue}</p>
-    `);
-  }
-
-  //gender func.
   function genderPrint() {
     divs[2].insertAdjacentHTML('beforeend', `
       <p>${genderSelectText}</p>
@@ -47,7 +53,14 @@ function pageLoad () {
     `);
   }
 
-  //class func.
+  genderSelect.addEventListener('change', genderPrint);
+
+
+  //class
+  let classSelect = document.getElementById("class-select");
+  let classSelectValue = classSelect.options[classSelect.selectedIndex].value;
+  let classSelectText = classSelect.options[classSelect.selectedIndex].text;
+
   function classPrint() {
     divs[3].insertAdjacentHTML('beforeend', `
       <p>${classSelectText}</p>
@@ -55,14 +68,11 @@ function pageLoad () {
     `);
   }
 
-  //ch name invite
-  chName.addEventListener('input', showChName);
-  //race invite
-  raceSelect.addEventListener('change', racePrint);
-  //gender invite
-  genderSelect.addEventListener('change', genderPrint);
-  //class invite
   classSelect.addEventListener('change', classPrint);
+
+
+  //submit variable
+  //let submit = document.getElementById('submit');
 }
 
 window.addEventListener('load', pageLoad);
